3673548d03b5952e8d50e2a1f634887d89282770
Love2104 <Lovechourasia2004@gmail.com>
2024-06-13 16:37:57 +0530

    ANC

---
+++ src/components/research.jsx
@@ -0,0 +1,484 @@
+import React from "react";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/about.jpg" className="img-responsive" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>About Us</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const Activities = (props) => {
+  return (
+    <div id="activities" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2> Our Activities</h2>
+          <p>
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+import { useState } from "react";
+import emailjs from "emailjs-com";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
+  
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, email, message);
+    
+    /* replace below with your own Service ID, Template ID and Public Key from your EmailJS account */ 
+    
+    emailjs
+      .sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", e.target, "YOUR_PUBLIC_KEY")
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearState();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Get In Touch</h2>
+                <p>
+                  Please fill out the form below to send us an email and we will
+                  get back to you as soon as possible.
+                </p>
+              </div>
+              <form name="sentMessage" validate onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Name"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Email"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Message"
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Address
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Phone
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+          <div className="col-md-12">
+            <div className="row">
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href={props.data ? props.data.facebook : "/"}>
+                      <i className="fa fa-facebook"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.instagram : "/"}>
+                      <i className="fa fa-instagram"></i>
+                    </a>
+                  </li>
+                  <li>
+                    <a href={props.data ? props.data.linkedin : "/"}>
+                    <i class="fa fa-linkedin"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
+            <a href="http://www.templatewire.com" rel="nofollow">
+              TemplateWire
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+import { Image } from "./image";
+
+export const Gallery = (props) => {
+  return (
+    <div id="portfolio" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Gallery</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className="row">
+          <div className="portfolio-items">
+            {props.data
+              ? props.data.map((d, i) => (
+                  <div
+                    key={`${d.title}-${i}`}
+                    className="col-sm-6 col-md-4 col-lg-4"
+                  >
+                    <Image
+                      title={d.title}
+                      largeImage={d.largeImage}
+                      smallImage={d.smallImage}
+                    />
+                  </div>
+                ))
+              : "Loading..."}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export const Header = (props) => {
+  return (
+    <header id="header">
+      <div className="intro">
+        <div className="overlay">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 col-md-offset-2 intro-text">
+                <h1>
+                  {props.data ? props.data.title : "Loading"}
+                  <span></span>
+                </h1>
+                <p>{props.data ? props.data.paragraph : "Loading"}</p>
+                <a
+                  href="#Objectives"
+                  className="btn btn-custom btn-lg page-scroll"
+                >
+                  Learn More
+                </a>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+    </header>
+  );
+};
+
+
+
+export const Images = ({ title, largeImage, smallImage }) => {
+  return (
+    <div className="portfolio-item">
+      <div className="hover-bg">
+        {" "}
+        <a href={largeImage} title={title} data-lightbox-gallery="gallery1">
+          <div className="hover-text">
+            <h4>{title}</h4>
+          </div>
+          <img src={smallImage} className="img-responsive" alt={title} />{" "}
+        </a>{" "}
+      </div>
+    </div>
+  );
+};
+
+
+
+export const Navigation = (props) => {
+  return (
+    <nav id="menu" className="navbar navbar-default navbar-fixed-top">
+      <div className="container">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
+          >
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">
+        Research Wing
+          </a>{" "}
+        </div>
+
+        <div
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
+        >
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="#Objectives" className="page-scroll">
+                Objectives
+              </a>
+            </li>
+            <li>
+              <a href="#about" className="page-scroll">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#Activities" className="page-scroll">
+              Our Activities
+              </a>
+            </li>
+            <li>
+              <a href="#portfolio" className="page-scroll">
+                Gallery
+              </a>
+            </li>
+            <li>
+              <a href="#testimonials" className="page-scroll">
+                Testimonials
+              </a>
+            </li>
+            <li>
+              <a href="#team" className="page-scroll">
+                Team
+              </a>
+            </li>
+            <li>
+              <a href="#contact" className="page-scroll">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+
+
+export const Objectives = (props) => {
+  return (
+    <div id="Objectives" className="text-center">
+      <div className="container">
+        <div className="col-md-10 col-md-offset-1 section-title">
+          <h2>Objectives</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                  <p>{d.text}</p>
+                </div>
+              ))
+            : "Loading..."}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const Team = (props) => {
+  return (
+    <div id="team" className="text-center">
+      <div className="container">
+        <div className="col-md-8 col-md-offset-2 section-title">
+          <h2>Meet the Team</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div id="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
+                  <div className="thumbnail">
+                    {" "}
+                    <img src={d.img} alt="..." className="team-img" />
+                    <div className="caption">
+                      <h4>{d.name}</h4>
+                      <p>{d.job}</p>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+
+export const Testimonials = (props) => {
+  return (
+    <div id="testimonials">
+      <div className="container">
+        <div className="section-title text-center">
+          <h2>What our clients say</h2>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  <div className="testimonial">
+                    <div className="testimonial-image">
+                      {" "}
+                      <img src={d.img} alt="" />{" "}
+                    </div>
+                    <div className="testimonial-content">
+                      <p>"{d.text}"</p>
+                      <div className="testimonial-meta"> - {d.name} </div>
+                    </div>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
